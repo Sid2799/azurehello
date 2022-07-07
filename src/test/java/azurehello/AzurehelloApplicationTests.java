e51f4a075d040cb3b3ab5adf623589493b17f8ff
Sid2799 <prembankar2706@gmail.com>
2022-07-07 22:48:11 +0530

    first commit

---
+++ src/test/java/azurehello/AzurehelloApplicationTests.java
@@ -0,0 +1,13 @@
+package azurehello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurehelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
